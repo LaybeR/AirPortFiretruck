89bec28ea182a331805efdf391c51fefaca8c7b8
LaybeR <layberize@gmail.com>
2022-02-23 15:20:37 +0100

    Initial commit

---
+++ src/main/java/ICentralUnit.java
@@ -0,0 +1,2 @@
+public interface ICentralUnit {
+}
---
+++ src/test/java/FireTruckTests.java
@@ -0,0 +1,16 @@
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class FireTruckTests {
+
+    @BeforeEach
+    void setup() {
+
+    }
+
+    @Test
+    void testCreation() {
+
+    }
+}
