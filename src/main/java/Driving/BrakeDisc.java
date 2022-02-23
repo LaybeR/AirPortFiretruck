5c587e2a74fd25b9530bed7904e2253e28150e84
Sasha <sasharo@gmx.net>
2022-02-23 15:52:12 +0100

    DRIVE

--- src/main/java/Driving/BrakeDisc.java
+++ src/main/java/Driving/BrakeDisc.java
@@ -1,4 +1,9 @@
 package Driving;
+import Enums.LeftRightPosition;
 
 public class BrakeDisc {
+    LeftRightPosition p;
+    BrakeDisc(LeftRightPosition pos){
+        this.p = pos;
+    }
 }
--- src/main/java/Driving/Pivot.java
+++ src/main/java/Driving/Pivot.java
@@ -1,13 +1,16 @@
 package Driving;
 
+import static Enums.LeftRightPosition.LEFT;
+import static Enums.LeftRightPosition.RIGHT;
+
 public abstract class Pivot {
     Pivot (){
-        BrakeDisc BD0 = new BrakeDisc();
-        BrakeDisc BD1 = new BrakeDisc();
-        BrakeDisc BD2 = new BrakeDisc();
-        BrakeDisc BD3 = new BrakeDisc();
-        BrakeDisc BD4 = new BrakeDisc();
-        BrakeDisc BD5 = new BrakeDisc();
+        BrakeDisc BD0 = new BrakeDisc(LEFT);
+        BrakeDisc BD1 = new BrakeDisc(RIGHT);
+        BrakeDisc BD2 = new BrakeDisc(LEFT);
+        BrakeDisc BD3 = new BrakeDisc(RIGHT);
+        BrakeDisc BD4 = new BrakeDisc(LEFT);
+        BrakeDisc BD5 = new BrakeDisc(RIGHT);
 
         Tire T1 = new Tire();
         Tire T2 = new Tire();
