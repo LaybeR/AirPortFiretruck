ffc3a09ed61ec308e92b9fa23a5aefefc6221cd7
Sasha <sasharo@gmx.net>
2022-02-23 16:03:23 +0100

    ...

--- src/main/java/Driving/Chassis.java
+++ src/main/java/Driving/Chassis.java
@@ -1,4 +1,19 @@
 package Driving;
 
 public class Chassis {
+    public void increaseSpeed(){
+
+    }
+    public void decreaseSpeed(){
+
+    }
+    public int getSpeed(){
+        return 0;
+    }
+    public void changeRotation(){
+
+    }
+    Chassis(){
+
+    }
 }
